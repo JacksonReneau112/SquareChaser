a541dc8e1f98b036c0fa5ed28ce329938d686dee
jackrene112 <jackrene112@ed.amdsb.ca>
2022-05-02 14:43:07 -0400

    Add project files.

---
+++ SquareChaser/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SquareChaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameEngine = new System.Windows.Forms.Timer(this.components);
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameEngine
+            // 
+            this.gameEngine.Enabled = true;
+            this.gameEngine.Interval = 20;
+            this.gameEngine.Tick += new System.EventHandler(this.gameEngine_Tick);
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.AutoSize = true;
+            this.player1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player1ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1ScoreLabel.ForeColor = System.Drawing.Color.Red;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(145, 24);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(24, 25);
+            this.player1ScoreLabel.TabIndex = 0;
+            this.player1ScoreLabel.Text = "0";
+            // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.AutoSize = true;
+            this.player2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player2ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2ScoreLabel.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(283, 24);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(24, 25);
+            this.player2ScoreLabel.TabIndex = 1;
+            this.player2ScoreLabel.Text = "0";
+            // 
+            // winLabel
+            // 
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("NSimSun", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(12, 24);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(450, 534);
+            this.winLabel.TabIndex = 2;
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winLabel.Visible = false;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(165, 390);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(128, 38);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "RESET";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Visible = false;
+            this.resetButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(474, 587);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Square Chaser";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameEngine;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ SquareChaser/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace SquareChaser
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(20, 283, 20, 20);
+        Rectangle player2 = new Rectangle(434, 283, 20, 20);
+        Rectangle whiteSquare;
+        Rectangle yellowCircle;
+        Rectangle redCircle;
+
+        //Sounds
+        SoundPlayer speedBoost = new SoundPlayer(Properties.Resources.speedBoost);
+        SoundPlayer ding = new SoundPlayer(Properties.Resources.ding);
+        SoundPlayer wrong = new SoundPlayer(Properties.Resources.wrong); 
+        SoundPlayer win = new SoundPlayer(Properties.Resources.win);
+
+        //Colours
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        Pen whitePen = new Pen(Color.White, 10);
+
+        bool wDown = false;
+        bool aDown = false;
+        bool sDown = false;
+        bool dDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftDown = false;
+        bool rightDown = false;
+
+        //Player Scores
+        int player1Score, player2Score;
+
+        //Player speeds
+        int player1Speed = 4;
+        int player2Speed = 4;
+
+        Random randGen = new Random();
+
+        int whiteSquareX, whiteSquareY, yellowCircleX, yellowCircleY, redCircleX, redCircleY;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            whiteSquareX = randGen.Next(15, 440);
+            whiteSquareY = randGen.Next(15, 553);
+            yellowCircleX = randGen.Next(15, 440);
+            yellowCircleY = randGen.Next(15, 553);
+            redCircleX = randGen.Next(15, 440);
+            redCircleY = randGen.Next(15, 553);
+
+            yellowCircle = new Rectangle(yellowCircleX, yellowCircleY, 10, 10);
+            whiteSquare = new Rectangle(whiteSquareX, whiteSquareY, 10, 10);
+            redCircle = new Rectangle(redCircleX, redCircleY, 10, 10);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+            }
+        }
+
+        private void gameEngine_Tick(object sender, EventArgs e)
+        {
+            //move player 1
+            if (wDown == true && player1.Y > 15)
+            {
+                player1.Y -= player1Speed;
+            }
+
+            if (sDown == true && player1.Y < 552)
+            {
+                player1.Y += player1Speed;
+            }
+
+            if (aDown == true && player1.X > 15)
+            {
+                player1.X -= player1Speed;
+            }
+
+            if (dDown == true && player1.X < 437)
+            {
+                player1.X += player1Speed;
+            }
+            //move player 2
+            if (upArrowDown == true && player2.Y > 15)
+            {
+                player2.Y -= player2Speed;
+            }
+
+            if (downArrowDown == true && player2.Y < 552)
+            {
+                player2.Y += player2Speed;
+            }
+
+            if (leftDown == true && player2.X > 15)
+            {
+                player2.X -= player2Speed;
+            }
+
+            if (rightDown == true && player2.X < 437)
+            {
+                player2.X += player2Speed;
+            }
+
+            //Player Makes contact with white square
+            if(player1.IntersectsWith(whiteSquare))
+            {
+                ding.Play();
+
+                player1Score++;
+                player1ScoreLabel.Text = $"{player1Score}";
+
+                whiteSquareX = randGen.Next(15, 440);
+                whiteSquareY = randGen.Next(15, 553);
+
+                whiteSquare = new Rectangle(whiteSquareX, whiteSquareY, 10, 10);
+            }
+            else if(player2.IntersectsWith(whiteSquare))
+            {
+                ding.Play();
+
+                player2Score++;
+                player2ScoreLabel.Text = $"{player2Score}";
+
+                whiteSquareX = randGen.Next(15, 440);
+                whiteSquareY = randGen.Next(15, 553);
+
+                whiteSquare = new Rectangle(whiteSquareX, whiteSquareY, 10, 10);
+            }
+           
+            //Player Makes Contact With Yellow Circle
+            if (player1.IntersectsWith(yellowCircle))
+            {
+                speedBoost.Play();
+
+                player1Speed = player1Speed + 2;
+
+                yellowCircleX = randGen.Next(15, 440);
+                yellowCircleY = randGen.Next(15, 553);
+
+                yellowCircle = new Rectangle(yellowCircleX, yellowCircleY, 10, 10);
+            }
+            else if (player2.IntersectsWith(yellowCircle))
+            {
+                speedBoost.Play();
+
+                player2Speed = player2Speed + 2;
+
+                yellowCircleX = randGen.Next(15, 440);
+                yellowCircleY = randGen.Next(15, 553);
+
+                yellowCircle = new Rectangle(yellowCircleX, yellowCircleY, 10, 10);
+            }
+
+            //Player Makes Contact with Red Circle
+            if (player1.IntersectsWith(redCircle))
+            {
+                wrong.Play();
+
+                player1Speed = player1Speed - 2;
+
+                redCircleX = randGen.Next(15, 440);
+                redCircleY = randGen.Next(15, 553);
+
+                redCircle = new Rectangle(redCircleX, redCircleY, 10, 10);
+            }
+            else if (player2.IntersectsWith(redCircle))
+            {
+                wrong.Play();
+
+                player2Speed = player2Speed - 2;
+
+                redCircleX = randGen.Next(15, 440);
+                redCircleY = randGen.Next(15, 553);
+
+                redCircle = new Rectangle(redCircleX, redCircleY, 10, 10);
+            }
+
+            //Player gets score of 5 and wins.
+            if(player1Score == 5)
+            {
+                win.Play();
+
+                gameEngine.Enabled = false;
+                winLabel.Visible = true;
+                //resetButton.Visible = true;
+                winLabel.Text = $"Player 1 Wins, {player1Score} - {player2Score}";
+            }
+            else if (player2Score == 5)
+            {
+                win.Play();
+
+                gameEngine.Enabled = false;
+                winLabel.Visible = true;
+                //resetButton.Visible = true;
+                winLabel.Text = $"Player 2 Wins, {player2Score} - {player1Score}";
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //Draws the graphics on the screen
+            e.Graphics.DrawRectangle(whitePen, 10, 10, 454, 567);
+            e.Graphics.FillRectangle(redBrush, player1);
+            e.Graphics.FillRectangle(blueBrush, player2);
+            e.Graphics.FillRectangle(whiteBrush, whiteSquare);
+            e.Graphics.FillEllipse(yellowBrush, yellowCircle);
+            e.Graphics.FillEllipse(redBrush, redCircle);
+        }        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+           //Reset Button
+            //player1Score = 0;
+            //player2Score = 0;
+            //winLabel.Visible = false;
+            //resetButton.Visible = false;
+            //gameEngine.Enabled = true;
+        }
+    }
+}
